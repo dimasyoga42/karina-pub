cd02393d0e8083a66beba0fdad72e8167b5014a8
dimasyoga42 <dimasyoga9090@gmail.com>
2025-10-06 18:49:23 +0700

    add search xtall

--- src/cmd/menuToram.js
+++ src/cmd/menuToram.js
@@ -3,8 +3,8 @@ import { getXtall } from "../plugin/xtallplug.js";
 export const toramMenu = async (text, sock, chatId, msg) => {
 try {
   if(text.startsWith(".xtall")) {
-    const arg = text.replace(".xtall", "");
-    getXtall(sock, chatId, msg, arg);
+    const wc = text.replace(".xtall", "");
+   await  getXtall(sock, chatId, msg, wc);
   }
 } catch (err) {
   console.log(err)
