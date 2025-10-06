cc8bef24ae8c487a5ae724c7bbeea7e8cf5c8428
dimasyoga42 <dimasyoga9090@gmail.com>
2025-10-06 18:56:49 +0700

    add search xtall

--- src/plugin/xtallplug.js
+++ src/plugin/xtallplug.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getXtall = async (sock, chatId, msg, name) => {
   try {
     const res = await axios.get(`https://toramonline.vercel.app/xtall/name/${name}`);
-    const data = res.data.data; // ✅ ambil array dari field "data"
+    const data = res.data; // ✅ ambil array dari field "data"
     console.log(data)
 
     if (!Array.isArray(data) || data.length === 0) {
