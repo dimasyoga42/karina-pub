31b4c61bf65a4c4a24fc5227329fb8c0ae1fbefb
dimasyoga42 <dimasyoga9090@gmail.com>
2025-10-06 18:55:46 +0700

    add search xtall

--- src/plugin/xtallplug.js
+++ src/plugin/xtallplug.js
@@ -4,6 +4,7 @@ export const getXtall = async (sock, chatId, msg, name) => {
   try {
     const res = await axios.get(`https://toramonline.vercel.app/xtall/name/${name}`);
     const data = res.data.data; // ✅ ambil array dari field "data"
+    console.log(data)
 
     if (!Array.isArray(data) || data.length === 0) {
       return sock.sendMessage(chatId, { text: "❌ Xtall tidak ditemukan!" });
