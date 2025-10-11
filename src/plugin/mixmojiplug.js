da975a2fdc99dc48324b8e75ed578f4639676751
dimasyoga42 <dimasyoga9090@gmail.com>
2025-10-11 11:11:22 +0700

    add fit qc

--- src/plugin/mixmojiplug.js
+++ src/plugin/mixmojiplug.js
@@ -6,9 +6,8 @@ const MAX_FILE_SIZE = 10 * 1024 * 1024; // 10MB
 
 export const Mix = async (sock, chatId, msg, argone, argtwo) => {
   try {
-    const mixmoji1 = argone.trim();
-    const mixmoji2 = argtwo.trim();
-
+    const mixmoji1 = argone
+    const mixmoji2 = argtwo
     // Request ke Tenor API
     const response = await fetch(
       `https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(mixmoji1)}_${encodeURIComponent(mixmoji2)}`
@@ -17,6 +16,7 @@ export const Mix = async (sock, chatId, msg, argone, argtwo) => {
     if (!response.ok) throw new Error(await response.text());
 
     const data = await response.json();
+    console.log({data});
 
     // Cek apakah ada results
 
