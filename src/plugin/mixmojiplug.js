fdf55f47ee1060d6945c349d78ce9181331d0f0a
dimasyoga42 <dimasyoga9090@gmail.com>
2025-10-11 18:08:55 +0700

    fix admin user

--- src/plugin/mixmojiplug.js
+++ src/plugin/mixmojiplug.js
@@ -14,35 +14,8 @@ export const Mix = async (sock, chatId, msg, argone, argtwo) => {
     if (!response.ok) throw new Error(`HTTP ${response.status}`);
 
     const data = await response.json();
-    const imageUrl = data.results[0].url;
-    console.log(data)
+    console.log(data);
 
-    if (!imageUrl) {
-      await sock.sendMessage(chatId, {
-        text: "❌ Kombinasi emoji tidak ditemukan atau tidak didukung 😔",
-      }, { quoted: msg });
-      return;
-    }
-
-    // Unduh gambar
-    const imgRes = await fetch(imageUrl);
-    const arrBuf = await imgRes.arrayBuffer();
-    const buffer = Buffer.from(arrBuf);
-
-    // Konversi ke webp sticker
-    const sticker = await sharp(buffer)
-      .resize(STICKER_SIZE, STICKER_SIZE, {
-        fit: "contain",
-        background: { r: 0, g: 0, b: 0, alpha: 0 },
-      })
-      .webp({ quality: WEBP_QUALITY })
-      .toBuffer();
-
-    await sock.sendMessage(
-      chatId,
-      { sticker },
-      { quoted: msg }
-    );
   } catch (err) {
     console.error("Mix error:", err);
     await sock.sendMessage(chatId, {
